21d6b1a352f933e19c935f916459060c8d7d7839
kareemmoheb <kmoheb92@gmail.com>
2022-04-19 00:40:51 +0200

    adding shelf changer

---
+++ src/Components/SelfSelector.js
@@ -0,0 +1,13 @@
+export default function ShelfSelector({ currentShelf, book, onChangeShelf }) {
+    return (
+        <div className="book-shelf-changer">
+            <select value={currentShelf} onChange={(e) => onChangeShelf(book, e.target.value)}>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+            </select>
+        </div>
+    );
+}
