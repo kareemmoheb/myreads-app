b0f3bb5302c70b0ac0639a170b1a886f8827e5bd
kareemmoheb <kmoheb92@gmail.com>
2022-04-19 00:40:05 +0200

    adding single book

---
+++ src/Components/Book.js
@@ -0,0 +1,21 @@
+import ShelfSelector from './SelfSelector';
+export default function Book({ book, onChangeShelf }) {
+    const title = book.title;
+    const cover = book.imageLinks.thumbnail;
+    const authors = book.authors;
+    const currentShelf = book.shelf;
+    return (
+        <li>
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${cover})` }}></div>
+                    <ShelfSelector currentShelf={currentShelf} book={book} onChangeShelf={onChangeShelf} />
+                </div>
+                <div className="book-title">{title}</div>
+                <div className="book-authors">
+                    {authors.map((author, index) => <span key={index}>{author} <br /></span>)}
+                </div>
+            </div>
+        </li>
+    );
+}
