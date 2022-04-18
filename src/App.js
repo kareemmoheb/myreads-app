f3a2e34857a48a16cd9896920aa270d9479eebf9
kareemmoheb <kmoheb92@gmail.com>
2022-04-18 21:47:19 +0200

    init app files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import BooksList from './Components/BooksList';
+import SearchBar from './Components/SearchBar';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="app">
+            <BooksList />
+            <Routes>
+                <Route path='/search' element={<SearchBar />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
